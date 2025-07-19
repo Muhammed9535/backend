e36abe4422cb1bd0f300cfb0b3f032f6d41fe8f7
unknown <lawalolarewaju123@gmail.con>
2025-07-19 08:16:44 +0100

    second commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ pool.connect()
 app.set("trust proxy", 1);
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['https://exclusive-ecommerce-admin.onrender.com', 'https://exclusive-ecommerce-frontend.onrender.com'],
     credentials: true  // Allow cookies in CORS
 }))
 
