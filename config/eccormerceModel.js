23b502f1cc95c32e5934fb738b74a77e289f5cb2
Muhammed9535 <105399837+Muhammed9535@users.noreply.github.com>
2025-07-19 21:00:21 +0100

    Update eccormerceModel.js

--- config/eccormerceModel.js
+++ config/eccormerceModel.js
@@ -6,12 +6,8 @@ const { Pool } = pg;
 
 
 const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT,
-  ssl: false,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 export default pool;
